f157cc053bede22a37b7fa5e04b22330e103ee89
Baris Sari <sariba@itu.edu.tr>
2020-12-24 17:04:54 +0300

    Run pre-commit hooks

--- src/bar/a.py
+++ src/bar/a.py
@@ -1,5 +1,6 @@
 def example():
     return "Hi from bar"
 
+
 def example_2():
     return "Hi again, this is from bar by the way!"
--- src/foo/a.py
+++ src/foo/a.py
@@ -1,10 +1,10 @@
 def example():
     return "Hi from foo"
 
+
 class A:
     def __init__(self, a="a"):
         self.a = a
-
 
     def print(self):
         print(self.a)
