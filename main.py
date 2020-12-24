6a93d7c08b4aad6fb37ca8c88c619707f2c95762
Baris Sari <sariba@itu.edu.tr>
2020-12-24 15:36:04 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from src.bar import a as bar_a
+import os
+from src.foo.a import example
+
+def main():
+    print("Let's violate maximum line length here. The default line length is 79 for flake8, but this one has more than 100 characters.")
+    print("Working directory: ", os.getcwd())
+    print(bar_a.example())
+    print(example())
+    print(bar_a.example_2())
+
+if __name__=="__main__":
+    main()
